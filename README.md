c20c1019b928974bc37d72d961a1d5220684f1be
AlessandroGranato <a.granato.89@gmail.com>
2024-01-01 19:12:03 +0100

    Feature - 01 - basic functions - create project skeleton - import springframework.cloud dependencies - import spring-cloud-starter-netflix-eureka-server dependency - setup needed properties

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# service-registry
+# Service registry
+Service registry module with Spring boot and Netflix Eureka Server technologies
---
+++ application/src/main/java/com/pyrosandro/serviceRegistry/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.pyrosandro.serviceRegistry.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
